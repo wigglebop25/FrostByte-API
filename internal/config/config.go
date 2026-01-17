1b6a89b4c9539c1256a65c9e62768355c0933536
wigglebop25 <jtaguiamwork@gmail.com>
2026-01-17 15:56:06 +0800

    fix(config): update DSN location to Asia/Manila for timezone consistency

--- internal/config/config.go
+++ internal/config/config.go
@@ -32,7 +32,7 @@ func LoadConfig() *Config {
 }
 
 func (c *Config) DSN() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia%%2FManila",
 		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
 }
 
